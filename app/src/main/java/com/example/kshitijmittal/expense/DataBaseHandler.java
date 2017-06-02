6e88e596abd3de036311bd0c1e72db66bc363694
Kshitij Mittal <kshitij.m1007@gmail.com>
2017-06-02 16:28:09 +0530

    databasehandle

--- app/src/main/java/com/example/kshitijmittal/expense/DataBaseHandler.java
+++ app/src/main/java/com/example/kshitijmittal/expense/DataBaseHandler.java
@@ -1,8 +1,23 @@
 package com.example.kshitijmittal.expense;
 
-/**
- * Created by Kshitij Mittal on 02-06-2017.
- */
 
-public class DataBaseHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseHandler extends SQLiteOpenHelper{
+
+    public DataBaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
